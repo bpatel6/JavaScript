592bfc44b05c4c1987c73a2cf97718a9c1593eee
bpatel6 <bpatel6@uiowa.edu>
2021-01-18 12:11:25 -0600

    script and implementation of the calculator

--- Practice/script.js
+++ Practice/script.js
@@ -1,85 +1,126 @@
-// App 1:
-
-function ageInDays() {
-    var birthYear = prompt('What year were you born?');
-    var ageInDays = (2021 - birthYear) * 365;
-    var h1 = document.createElement('h1');
-    var textAnswer = document.createTextNode('You are ' + ageInDays + ' days old');
-    h1.setAttribute('id', 'ageInDays');
-    h1.appendChild(textAnswer);
-    document.getElementById('flex-box-result').appendChild(h1);
-}
-
-function reset() {
-    document.getElementById('flex-box-result').remove();
-}
-
-// App 2:
-
-function rpsGame(input) {
-    console.log(input);
-    let humanChoice, botChoice;
-    humanChoice = input.id;
-    botChoice = numberToChoose(randToRPS());
-    let result = decideWinner(humanChoice, botChoice);
-    console.log(result);
-    message = finalMessage(result[0], result[1]);
-    console.log(message);
-    rpsFrontEnd(humanChoice, botChoice, message)
-
-}
-
-function randToRPS() {
-    return Math.floor(Math.random() * 3);
-}
-
-function numberToChoose(number) {
-    return ['rock', 'paper', 'scissors'][number];
-}
-
-function decideWinner(userInput, computerInput) {
-    let rpsData = {
-        'rock': {'scissors': 1, 'rock': 0.5, 'paper': 0},
-        'paper': {'rock': 1, 'paper': 0.5, 'scissors': 0},
-        'scissors': {'paper': 1, 'scissors': 0.5, 'rock': 0}
-    }
-    var yourScore = rpsData[userInput][computerInput];
-    var computerScore = rpsData[computerInput][userInput];
-    return [yourScore, computerScore];
-}
-
-function finalMessage(userScore, computerScore) {
-    if (userScore === 0) {
-        return {'message': 'You Lost!', 'color': 'red'};
-    } else if (userScore === 0.5) {
-        return {'message': 'You Tied!', 'color': 'yellow'};
-    } else {
-        return {'message': 'You Won!', 'color': 'green'};
-    }
-}
-
-function rpsFrontEnd(userImg, computerChoice, finalMessage) {
-    var imageDatabase = {
-        'rock': document.getElementById('rock').src,
-        'paper': document.getElementById('paper').src,
-        'scissors': document.getElementById('scissors').src
+class Calculator {
+    constructor(previousOperandTextElement, currentOperandTextElement) {
+        this.previousOperandTextElement = previousOperandTextElement
+        this.currentOperandTextElement = currentOperandTextElement
+        this.clear()
     }
 
-    document.getElementById('rock').remove();
-    document.getElementById('paper').remove();
-    document.getElementById('scissors').remove();
+    clear() {
+        this.currentOperand = ''
+        this.previousOperand = ''
+        this.operation = undefined
+    }
 
-    var userDiv = document.createElement('div');
-    var botDiv = document.createElement('div');
-    var message = document.createElement('div');
+    delete() {
+        this.currentOperand = this.currentOperand.toString().slice(0, -1)
+    }
 
-    userDiv.innerHTML = "<img src='" + imageDatabase[userImg] + "' height=150 width=150 style='box-shadow: 0px 10px 50px rgb(141,143,191)'>";
-    message.innerHTML = "<h1 style='color: " + finalMessage['color'] + "; font-size: 60px; padding: 30px; '>" + finalMessage['message'] + "</h1>";
-    botDiv.innerHTML = "<img src='" + imageDatabase[computerChoice] + "' height=150 width=150 style='box-shadow: 0px 10px 50px rgb(81,81,96)'>";
+    appendNumber(number) {
+        if (number === '.' && this.currentOperand.includes('.')) return
+        this.currentOperand = this.currentOperand.toString() + number.toString()
+    }
 
-    document.getElementById('flex-box-rps-div').appendChild(userDiv);
-    document.getElementById('flex-box-rps-div').appendChild(message);
-    document.getElementById('flex-box-rps-div').appendChild(botDiv);
+    chooseOperation(operation) {
+        if (this.currentOperand === '') return
+        if (this.previousOperand !== '') {
+            this.compute()
+        }
+        this.operation = operation
+        this.previousOperand = this.currentOperand
+        this.currentOperand = ''
+    }
 
+    compute() {
+        let computation
+        const prev = parseFloat(this.previousOperand)
+        const current = parseFloat(this.currentOperand)
+        if (isNaN(prev) || isNaN(current)) return
+        switch (this.operation) {
+            case '+':
+                computation = prev + current
+                break
+            case '-':
+                computation = prev - current
+                break
+            case '*':
+                computation = prev * current
+                break
+            case '÷':
+                computation = prev / current
+                break
+            default:
+                return
+        }
+        this.currentOperand = computation
+        this.operation = undefined
+        this.previousOperand = ''
+    }
 
+    getDisplayNumber(number) {
+        const stringNumber = number.toString()
+        const integerDigits = parseFloat(stringNumber.split('.')[0])
+        const decimalDigits = stringNumber.split('.')[1]
+        let integerDisplay
+        if (isNaN(integerDigits)) {
+            integerDisplay = ''
+        } else {
+            integerDisplay = integerDigits.toLocaleString('en', {maximumFractionDigits: 0})
+        }
+        if (decimalDigits != null) {
+            return `${integerDisplay}.${decimalDigits}`
+        } else {
+            return integerDisplay
+        }
+    }
+
+    updateDisplay() {
+        this.currentOperandTextElement.innerText =
+            this.getDisplayNumber(this.currentOperand)
+        if (this.operation != null) {
+            this.previousOperandTextElement.innerText =
+                `${this.getDisplayNumber(this.previousOperand)} ${this.operation}`
+        } else {
+            this.previousOperandTextElement.innerText = ''
+        }
+    }
 }
+
+
+const numberButtons = document.querySelectorAll('[data-number]')
+const operationButtons = document.querySelectorAll('[data-operation]')
+const equalsButton = document.querySelector('[data-equals]')
+const deleteButton = document.querySelector('[data-delete]')
+const allClearButton = document.querySelector('[data-all-clear]')
+const previousOperandTextElement = document.querySelector('[data-previous-operand]')
+const currentOperandTextElement = document.querySelector('[data-current-operand]')
+
+const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement)
+
+numberButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.appendNumber(button.innerText)
+        calculator.updateDisplay()
+    })
+})
+
+operationButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.chooseOperation(button.innerText)
+        calculator.updateDisplay()
+    })
+})
+
+equalsButton.addEventListener('click', button => {
+    calculator.compute()
+    calculator.updateDisplay()
+})
+
+allClearButton.addEventListener('click', button => {
+    calculator.clear()
+    calculator.updateDisplay()
+})
+
+deleteButton.addEventListener('click', button => {
+    calculator.delete()
+    calculator.updateDisplay()
+})
